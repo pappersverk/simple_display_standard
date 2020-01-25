984536fc4eb7f4763e7b8106aa3ab5e2bf6a9970
Lars Wikman <lars.wikman@gmail.com>
2020-01-25 23:57:44 +0100

    Clean up info return format to match others

--- lib/simple_display.ex
+++ lib/simple_display.ex
@@ -4,9 +4,11 @@ defmodule SimpleDisplay do
   target for a lot of simpler display in Elixir and Nerves.
   """
 
-  @callback info(pid()) :: %{
-              resolution: {integer(), integer()}
-            }
+  @callback info(pid()) ::
+              {:ok,
+               %{
+                 resolution: {integer(), integer()}
+               }}
   @callback set_pixels(pid(), [byte()]) :: :ok | {:error, Atom.t(), String.t()}
   @callback render_to_display(pid()) :: :ok | {:error, Atom.t(), String.t()}
 end
